9820667821473ee8855737578a8f58f9499ab000
remyCases <remy.cases@gmail.com>
2024-07-16 15:04:35 +0200

    [Major] it's alive!

---
+++ Plugin.cs
@@ -0,0 +1,135 @@
+﻿// Copyright (C) 2024 Rémy Cases
+// See LICENSE file for extended copyright information.
+// This file is part of the Speedshard repository from https://github.com/remyCases/TheIronOath-AllyFreeze.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using HarmonyLib.Tools;
+
+namespace AllyFreeze;
+
+public static class PluginInfo
+{
+    public const string PLUGIN_GUID = "AllyFreeze";
+    public const string PLUGIN_NAME = "AllyFreeze";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Log;
+    private void Awake()
+    {
+        Log = base.Logger;
+        // Plugin startup logic
+        HarmonyFileLog.Enabled = true;
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        Harmony harmony = new(PluginInfo.PLUGIN_GUID);
+        harmony.PatchAll();
+    }
+}
+
+[HarmonyDebug]
+[HarmonyPatch]
+public static class AllyFreeze
+{
+    [HarmonyTranspiler]
+    [HarmonyPatch(typeof(BaseClass))]
+    [HarmonyPatch(nameof(BaseClass.AddDebuff), new System.Type[] { typeof(DebuffBase), typeof(bool) })]
+    static IEnumerable<CodeInstruction> BaseClassAddDebuff(IEnumerable<CodeInstruction> instructions, ILGenerator il)
+    {       
+        bool areWeImmuneFound = false;
+        bool patchingDone = false;
+        bool labelTrueFix = false;
+        bool labelFalseFix = false;
+
+        Label labelFalse = il.DefineLabel();
+        Label labelTrue = il.DefineLabel();
+
+        FieldInfo debuffFld = typeof(BaseClass)
+            .GetNestedTypes(AccessTools.all)
+            .Where(t => t.Name.Contains("Display"))
+            .SelectMany(AccessTools.GetDeclaredFields)
+            .FirstOrDefault(m => m.Name.Contains("debuff"));
+
+        foreach(CodeInstruction instruction in instructions)
+        {
+            if (!areWeImmuneFound && instruction.Calls(AccessTools.Method(typeof(BaseClass), nameof(BaseClass.AreWeImmune), new System.Type[] { typeof(ResistType) })))
+            {
+                areWeImmuneFound = true;
+            }
+            else if (!patchingDone && areWeImmuneFound && instruction.opcode == OpCodes.Brfalse)
+            {
+                yield return new CodeInstruction(OpCodes.Brfalse_S, labelFalse);
+                yield return new CodeInstruction(OpCodes.Ldarg_2);
+                yield return new CodeInstruction(OpCodes.Brfalse_S, labelTrue);
+                yield return new CodeInstruction(OpCodes.Ldloc_0);
+                yield return new CodeInstruction(OpCodes.Ldfld, debuffFld);
+                yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(BuffBase), "Type"));
+                yield return new CodeInstruction(OpCodes.Ldc_I4_S, 15);
+                yield return new CodeInstruction(OpCodes.Beq_S, labelFalse);
+
+                patchingDone = true;
+                continue;
+            }
+            else if (!labelTrueFix && patchingDone && instruction.opcode == OpCodes.Ldarg_0)
+            {
+                instruction.labels.Add(labelTrue);
+                labelTrueFix = true;
+            }
+            else if (!labelFalseFix && labelTrueFix && instruction.opcode == OpCodes.Ldarg_0)
+            {
+                instruction.labels.Add(labelFalse);
+                labelFalseFix = true;
+            }
+            yield return instruction;
+        }
+    }
+
+    [HarmonyTranspiler]
+    [HarmonyPatch(typeof(Pugilist))]
+    [HarmonyPatch(nameof(Pugilist.AddDebuff), new System.Type[] { typeof(DebuffBase), typeof(bool) })]
+    static IEnumerable<CodeInstruction> PugilistAddDebuff(IEnumerable<CodeInstruction> instructions, ILGenerator il)
+    {       
+        bool patchingDone = false;
+        bool labelTrueFix = false;
+        bool labelFalseFix = false;
+
+        Label labelFalse = il.DefineLabel();
+        Label labelTrue = il.DefineLabel();
+
+        foreach(CodeInstruction instruction in instructions)
+        {
+            if (!patchingDone && instruction.opcode == OpCodes.Bne_Un)
+            {
+                yield return new CodeInstruction(OpCodes.Beq, labelTrue);
+                yield return new CodeInstruction(OpCodes.Ldarg_2);
+                yield return new CodeInstruction(OpCodes.Brfalse_S, labelFalse);
+                yield return new CodeInstruction(OpCodes.Ldarg_1);
+                yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(BuffBase), "Type"));
+                yield return new CodeInstruction(OpCodes.Ldc_I4_S, 15);
+                yield return new CodeInstruction(OpCodes.Bne_Un, labelFalse);
+
+                patchingDone = true;
+                continue;
+            }
+            else if (!labelTrueFix && patchingDone && instruction.opcode == OpCodes.Ldarg_0)
+            {
+                instruction.labels.Add(labelTrue);
+                labelTrueFix = true;
+            }
+            else if (!labelFalseFix && labelTrueFix && instruction.opcode == OpCodes.Ldarg_0)
+            {
+                instruction.labels.Add(labelFalse);
+                labelFalseFix = true;
+            }
+            yield return instruction;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,57 @@
+# The Iron Oath - AllyFreeze
+
+## Description
+
+FrozenTomb will always work on allies even if they have the `Immunity` buff or `Mind Over Matter` passive.
+
+## Installation
+
+This mod was made with `BepInEx` which is why you need to install it first.
+
+### Installing BepInEx
+
+- Download the version `5.4.22` of [BepInEx](https://github.com/BepInEx/BepInEx/releases/tag/v5.4.22).
+- Unzip it in the `The Iron Oath` folder.
+
+### Installing BiggerTeam
+
+- Download the latest release of [AllyFreeze](https://github.com/remyCases/TheIronOath-AllyFreeze/releases). 
+- Move it in the `The Iron Oath/BepInEx/plugins` folder.
+
+You can now play the game with the modded version !
+
+## Troubleshooting
+
+If you encountered some troubles while playing with this mod, you can contact me on [Discord](https://discord.com/users/200330865522376704), and send me the log file `LogOutput` found in the folder `The Iron Oath/BepInEx`.
+
+## For anyone who wants to change this mod and build it themself
+
+> [!CAUTION]
+> The current repo can not be built directly as I didn't include the `Assembly dll` of The Iron Oath (for obvious reasons).
+> You need to create a `lib` folder first and to move the `Assembly-CSharp.dll` from the `The Iron Oath/The Iron Oath_Data/Managed` folder to the `lib` folder.
+
+This mod was made using `HarmonyX`, a fork of `Harmony2` from `BepInEx`. I strongly advise anyone who wants to change the mod to check the documentation of [Harnomy2](https://harmony.pardeike.net/articles/intro.html) and to check the difference between [Harmony2 and HarmonyX](https://github.com/BepInEx/HarmonyX/wiki/Difference-between-Harmony-and-HarmonyX) first.
+
+## See also
+
+Other mods I've made:
+- Stoneshard:
+    - [Speedshard_Core](https://github.com/remyCases/SpeedshardCore)
+    - [Speedshard_Sprint](https://github.com/remyCases/SpeedshardSprint)
+    - [Speedshard_Skinning](https://github.com/remyCases/SpeedshardSkinning)
+    - [Speedshard_MoneyDungeon](https://github.com/remyCases/SpeedshardMoneyDungeon)
+    - [Speedshard_Stances](https://github.com/remyCases/SpeedshardStances)
+    - [Pelt Durability](https://github.com/remyCases/Stoneshard-PeltDurability)
+    - [MoreSaveSlots](https://github.com/remyCases/Stoneshard-MoreSaveSlots)
+    - [Defeat Scenarios](https://github.com/remyCases/Stoneshard-DefeatScenarios)
+    - [Guaranteed Uniques](https://github.com/remyCases/GuaranteedUniques)
+    - [Character Creator](https://github.com/remyCases/CharacterCreator)
+
+- Shardpunk:
+    - [Shardpunk-BiggerTeam](https://github.com/remyCases/Shardpunk-BiggerTeam)
+    - [Shardpunk-Faster](https://github.com/remyCases/Shardpunk-Faster)
+    - [Shardpunk-MoreSkillLevels](https://github.com/remyCases/Shardpunk-MoreSkillLevels)
+    - [Shardpunk-RandomParty](https://github.com/remyCases/Shardpunk-RandomParty)
+
+- Airship Kingdom Adrift:
+    - [ProductionPanel](https://github.com/remyCases/AKAMod_ProdPanel)
